21a9ab71bae1de27eca9be06f4425a3c2c0bc904
Greg <gregbanham@hotmail.com>
2020-09-30 15:28:30 +0100

    m

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.WebDriver;
 public class HomePage extends WebPage {
 
     private final String BASE_URL = Dotenv.load().get("BASE_URL");
-    //private final String PAGE_TITLE = "My Store";
+    private final String PAGE_TITLE = "My Store";
 
     public HomePage (WebDriver Driver)
     {
         super(Driver);
     }
 
-    //public String getExpectedPageTitle()
-//    {
-//        return PAGE_TITLE;
-//    }
+    public String getExpectedPageTitle()
+    {
+        return PAGE_TITLE;
+    }
 
     public void loadSite()
     {
