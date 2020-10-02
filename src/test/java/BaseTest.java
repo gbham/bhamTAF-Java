357c2fce3518944cd0f737a0b2ab0c0b7537f395
Greg <gregbanham@hotmail.com>
2020-10-02 16:37:10 +0100

    m

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -109,7 +109,9 @@ public class BaseTest {
         TakesScreenshot TS = ((TakesScreenshot)Driver);
 
         //File srcFile = TS.getScreenshotAs(OutputType.FILE);
-        File srcFileTest = new File("/dev/srcTestFile.txt");
+        //File srcFileTest = new File("/dev/srcTestFile.txt");
+        File srcFileTest = new File("/dev" + File.separator + "srcTestFile.txt");
+
         srcFileTest.createNewFile();
 
 //        File destFile = new File(String.format("%1$s\\Screenshots\\%2$s.png", TEST_DIR, testResult.getName()));
