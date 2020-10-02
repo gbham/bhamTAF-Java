bbbee672588c400ae06e30713d84a71775e28dd9
Greg <gregbanham@hotmail.com>
2020-10-02 14:29:30 +0100

    added screenshot after tests and place them in test directory

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,9 @@
+import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -7,26 +11,43 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.*;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pages.HomePage;
 import pages.Menu;
-import io.github.cdimascio.dotenv.Dotenv;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BaseTest {
 
     protected WebDriver Driver;
+    protected String TEST_DIR;
 
     protected String BROWSER_TYPE = Dotenv.load().get("BROWSER_TYPE");
     protected String SELENIUM_GRID = Dotenv.load().get("SELENIUM_GRID");
     protected String SELENIUM_HUB_URL = Dotenv.load().get("SELENIUM_HUB_URL");
 
+    @BeforeSuite
+    public void createTestDirectory() throws MalformedURLException
+    {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy-HH-mm-ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        TEST_DIR = String.format("C:\\Dev\\TestResults\\%s", dtf.format(now));
+
+        File file = new File(TEST_DIR);
+        file.mkdir();
+    }
+
     @BeforeMethod
-    public void SetUpDriver() throws MalformedURLException
+    public void setUpDriver() throws MalformedURLException
     {
         if(SELENIUM_GRID.equals("true"))
         {
@@ -39,9 +60,9 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void cleanUp() throws IOException
+    public void cleanUp(ITestResult testResult) throws IOException
     {
-        this.takeScreenshot();
+        this.takeScreenshot(testResult);
         Driver.quit();
     }
 
@@ -80,18 +101,16 @@ public class BaseTest {
         return Driver;
     }
 
-    public void takeScreenshot() throws IOException
+    public void takeScreenshot(ITestResult testResult) throws IOException
     {
         TakesScreenshot TS = ((TakesScreenshot)Driver);
 
         File srcFile = TS.getScreenshotAs(OutputType.FILE);
-        File destFile = new File("C:\\Dev\\Screenshots");
+
+        File destFile = new File(String.format("%1$s\\Screenshots\\%2$s.png", TEST_DIR, testResult.getName()));
 
         FileUtils.copyFile(srcFile, destFile);
-
     }
-
-
 
     public Menu loadSite()
     {
