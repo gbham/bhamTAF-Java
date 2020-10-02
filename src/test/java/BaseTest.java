7254cf1d8163902987258ca5a4fe0f6f0869e173
gbham <57295806+gbham@users.noreply.github.com>
2020-10-02 19:18:33 +0100

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -113,7 +113,7 @@ public class BaseTest {
         File srcFileTest = new File("srcTestFile.txt");
         //File srcFileTest = new File("/dev" + File.separator + "srcTestFile.txt");
         //File srcFileTest = new File("/srcTestFile.txt");
-        //srcFileTest.createNewFile();
+        srcFileTest.createNewFile();
 
 //        File destFile = new File(String.format("%1$s\\Screenshots\\%2$s.png", TEST_DIR, testResult.getName()));
         //File destFileTest = new File("/tmp/destTestFile.png");
