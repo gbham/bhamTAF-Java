8971c22c34e1f15ab3b4f579094ef38fb0b0886b
Greg <gregbanham@hotmail.com>
2020-10-02 16:30:36 +0100

    m

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -110,6 +110,7 @@ public class BaseTest {
 
         //File srcFile = TS.getScreenshotAs(OutputType.FILE);
         File srcFileTest = new File("/dev/srcTestFile.txt");
+        srcFileTest.createNewFile();
 
 //        File destFile = new File(String.format("%1$s\\Screenshots\\%2$s.png", TEST_DIR, testResult.getName()));
         File destFileTest = new File("/dev/destTestFile.txt");
